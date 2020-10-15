41cc09e3667e32b02045195b785cb668642fc463
droidsamples <72753283+droidsamples@users.noreply.github.com>
2020-10-15 12:51:25 +0530

    Update and rename BottomsheetDialog.kt to GenericBottomSheetDialog.kt

---
+++ GenericBottomSheetDialog.kt
@@ -0,0 +1,47 @@
+
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
+
+open class GenericBottomSheetDialog : DialogFragment() {
+
+    private var maxHeight = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_TITLE, R.style.CustomBottomSheetDialog)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        maxHeight = ((resources.displayMetrics.heightPixels) / 1.2).toInt()
+        dialog?.window?.setGravity(Gravity.BOTTOM)
+        dialog?.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog?.window?.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        setViewTreeObserver(view)
+    }
+
+
+    private fun setViewTreeObserver(view: View?) {
+        view?.viewTreeObserver?.addOnGlobalLayoutListener(
+            object : ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    view.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    if (view.height >= maxHeight) {
+                        view.layoutParams.height = maxHeight
+                        dialog?.window?.setLayout(
+                            WindowManager.LayoutParams.MATCH_PARENT,
+                            maxHeight
+                        )
+                    }
+                }
+            }
+        )
+    }
+}
